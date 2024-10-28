d0ae5e5f5a91e923f429c859fa954626cb1e71c5
Robert Wenink <robertwenink@gmail.com>
2024-10-28 12:53:08 +0100

    change colormap size to 510

--- create_colormap_csv.py
+++ create_colormap_csv.py
@@ -2,7 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Number of samples
-n = 1024
+# NB with 1024 we get repetitions in pairs, the max number is 512 in matplotlib, 
+# except that the first and last are also repeated so actually 510, 
+# which coincides with a scale of 256 colors (or values 0 to 255), now values 0 to 510;
+# these are 511 integer values, correct for this at the cpp end to only have unique values
+n = 510
 
 # Get the matplotlib twilight colormap
 cmap = plt.get_cmap('twilight', n)
